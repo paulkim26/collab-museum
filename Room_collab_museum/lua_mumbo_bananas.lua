d0d331b7ac27a00250baaa4e545b0c4eb801e054
chazzybuch <135690926+chazzybuch@users.noreply.github.com>
2023-06-11 09:53:45 -0400

    Door logic polish & Lua removal (#89)
    - Cleaned up door/render logic (Science still needs minor work re: initial opening sequence in multiplayer)
    - Fixed #84 (Science tunnel ambience)
    - Fixed #83 (wing doors visible in atrium)
    - Removed 3 Lua scripts and replaced functions using Empty logic #88
    - Slightly increased wing lock slot scale, adjusted slot position for keys to properly align with locks
    - Added visibility activator logic for Science meta puzzle Planet Info Cards for slight performance boost
    - Creator Card Rotation Lua script is only triggered when players take the elevator up. It is deactivated again when they take it back down.

--- Room_collab_museum/lua_mumbo_bananas.lua
+++
@@ -1,6 +0,0 @@
-if callType == LuaCallType.Init then
-    --api.log("Bananas loaded")
-
-elseif callType == LuaCallType.Update then
-	mumbo_banana_spinner.transform.Rotate(-60*Vector3.up * Time.deltaTime)
-end
