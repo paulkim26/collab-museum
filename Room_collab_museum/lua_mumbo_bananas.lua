25cdc4e96c207f9774beb3da25a1f179f5571c7f
Paul Kim <45007945+paulkim26@users.noreply.github.com>
2023-06-10 20:30:43 -0400

    Added section B exterior + fixed floor (#77)
    * Added section B exterior + fixed floor
    
    * Fixed Z06 + added hints + cleaned up banana log
    
    * Decimated whale skeleton
    
    69k to 6k faces
    
    * Fixed section B exterior now showing/hiding properly
    
    * Fixed bug where B exterior shows up on 1st floor
    
    Repro:
    -access B
    -access A
    -go back to access B
    => Section B exterior shows up on 1st floor
    
    * Fixed skyscraper transparency issue

--- Room_collab_museum/lua_mumbo_bananas.lua
+++ Room_collab_museum/lua_mumbo_bananas.lua
@@ -1,5 +1,5 @@
 if callType == LuaCallType.Init then
-    api.log("Bananas loaded")
+    --api.log("Bananas loaded")
 
 elseif callType == LuaCallType.Update then
 	mumbo_banana_spinner.transform.Rotate(-60*Vector3.up * Time.deltaTime)
