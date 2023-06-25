f1bae2a52688a130e3c0c1e49ba0bcf267cbe8c8
Paul Kim <paulkim26.pk@gmail.com>
2023-06-25 09:28:10 -0400

    Added section B tokens + hide debug buttons by default
    Added section B tokens
    Hide debug buttons by default
    Temporarily disabled token sound + log until ready
    Added temporary finish until balloon is ready
    Refactored token script lua_chazzyb_tokens.lua
    Fixed walkthrough truncation
    Fixed bug where section load triggers activate on 2nd floor

--- Room_collabmuseum/lua_chazzyb_tokens.lua
+++ Room_collabmuseum/lua_chazzyb_tokens.lua
@@ -1,85 +1,83 @@
+token_message_enable = false; --Set this to true when fix is ready
+token_count_max = 16;
+
+function collectToken()
+    token_count = token_count + 1;
+	
+	if token_message_enable then
+		api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/"..token_count_max.." found]");
+	end
+end
+
 if callType == LuaCallType.Init then
 	token_count = 0;
+	
 
 elseif callType == LuaCallType.TriggerExit then
+
+
     if context == token01 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+		collectToken();
 	end
 
     if context == token02 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token03 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token04 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token05 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token06 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token07 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token08 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token09 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token10 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token11 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token12 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token13 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token14 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token15 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
     if context == token16 then
-        token_count = token_count + 1;
-	api.levelNote("\bYou found a token!\nTokens can be spent in the gift shop, found in the atrium.\n["..token_count.."/16 found]");
+        collectToken();
 	end
 
 end
